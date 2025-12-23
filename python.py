e33a5be53ef555660a9011a1e94b773fc63b08e7
BalikaRahul <rr7511489@gmail.com>
2025-12-23 09:22:58 +0530

    day 5

--- python.py
+++ python.py
@@ -1,10 +1,11 @@
-def rahul(a,b):
-    for i in range(len(a)-1):
-        if a[i]==a[i+1]:
-            a=[i]+1
-        else:
-            b=b.append(a[i])
-a=[1,2,3,4,5]
-b=[]
-result =rahul(a,b)
+def leftRotate(arr):
+    n=int(input("enter the number:"))
+    k=n
+    for i in range(len(arr)-1):
+        arr[i]=arr[i+1]
+        arr[-i]=arr[i]
+    return arr
+
+arr=[1,2,3,4,5]
+result=leftRotate(arr)
 print(result)

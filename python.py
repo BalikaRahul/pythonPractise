c75eaa175162936141b5c0306b1d209af2cb56bb
BalikaRahul <rr7511489@gmail.com>
2025-12-25 10:11:32 +0530

    planning future events

--- python.py
+++ python.py
@@ -1,11 +1,1 @@
-def missingNumber(a,m):
-    sum =0
-    for i in range(len(a)):
-        sum+=a[i]
-    n=m*(m+1)//2
-    miss=n-sum
-    return miss
-a=[1,2,3,4,5,7]
-m=len(a)+1
-result = missingNumber(a,m)
-print(result)
+# plan done for future Events 

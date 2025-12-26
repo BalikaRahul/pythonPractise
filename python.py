0ace277dedb5b2bb8dbded7ead8e2d78fafc8c98
BalikaRahul <rr7511489@gmail.com>
2025-12-26 08:58:58 +0530

    day 8

--- python.py
+++ python.py
@@ -1,1 +1,10 @@
-# plan done for future Events 
+nums= [1,1,1,1,0,0,0,1,1,1]
+maxi=0
+count=0
+for i in nums:
+    if i ==1:
+        count+=1
+        maxi=max(maxi,count)
+    else:
+        count=0
+print(maxi)

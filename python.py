d9258262f30d0721ee7a5d0078ef53f2d027cc7a
BalikaRahul <rr7511489@gmail.com>
2025-12-27 14:58:02 +0530

    day9

--- python.py
+++ python.py
@@ -1,10 +1,14 @@
-nums= [1,1,1,1,0,0,0,1,1,1]
-maxi=0
-count=0
-for i in nums:
-    if i ==1:
-        count+=1
-        maxi=max(maxi,count)
-    else:
-        count=0
-print(maxi)
+class Student:
+    def __init__(self, name, marks):
+        self.name=name
+        self.marks= marks
+    
+    def avg(self):
+        sum =0
+        for i in self.marks:
+            sum+=i
+        print("hii",self.name,"your score is:",sum/len(self.marks))
+s1=Student("Rahul",[98,94,84])
+s2 = Student("Bhanu",[85,85,86,99])
+s2.avg()
+s1.avg()

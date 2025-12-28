178fe16e420ff537aa4a99ee8d3462ab0563075c
BalikaRahul <rr7511489@gmail.com>
2025-12-28 11:48:15 +0530

    day 10

--- python.py
+++ python.py
@@ -1,14 +1,1 @@
-class Student:
-    def __init__(self, name, marks):
-        self.name=name
-        self.marks= marks
-    
-    def avg(self):
-        sum =0
-        for i in self.marks:
-            sum+=i
-        print("hii",self.name,"your score is:",sum/len(self.marks))
-s1=Student("Rahul",[98,94,84])
-s2 = Student("Bhanu",[85,85,86,99])
-s2.avg()
-s1.avg()
+# learning the basics of backend .what is backend? . what is HTTP request and response ?. who is client and server....etc

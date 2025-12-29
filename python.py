4146890c4c40d5770804bea3b8d4be343c808a53
BalikaRahul <rr7511489@gmail.com>
2025-12-29 10:32:53 +0530

    day 11

--- python.py
+++ python.py
@@ -1,1 +1,9 @@
-# learning the basics of backend .what is backend? . what is HTTP request and response ?. who is client and server....etc
+#what is HTTP. what is client and sever . what is the difference between GET POST . what is the request and response . what are the types of requests.
+def even(num):
+    if(num%2==0):
+        return "even"
+    else:
+        return "not a even number"
+num =int(input("enter the number"))
+result= even(num)
+print(result)

1b2a34ca168d670b279853d97ae4f68d9cdbd991
BalikaRahul <rr7511489@gmail.com>
2026-01-02 14:44:43 +0530

    day 14

--- python.py
+++ python.py
@@ -1,8 +1,1 @@
-#find the youtube channel to learn backend 
-#today url building , dynamic pass  and conditional route completed 
-def even(num):
-    for i in range(0,num,2):
-        print(i)
-num=int(input("enter the number"))
-result=even(num)
-print(result)
+# learning django  first 1 hour complete without any doubt 

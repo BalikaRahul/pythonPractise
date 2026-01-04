32e0c9bde78ff0663e3f2c3006b31998333e635b
BalikaRahul <rr7511489@gmail.com>
2026-01-04 15:51:19 +0530

    day 16

--- python.py
+++ python.py
@@ -1,2 +1,2 @@
-print((lambda number: "even" if number % 2 == 0 else "odd")(6))
+
 # today i have learn what is static file , how to render bootstrap , how to create a app in python  , how to create super user for admin panel

9ffbf3dd6c4d788e1b483b90e86fd91fc452e4d3
BalikaRahul <rr7511489@gmail.com>
2026-01-07 10:30:40 +0530

    day 18

--- python.py
+++ python.py
@@ -1,7 +1,2 @@
 
-# today i have learn what about spread operation ,shortcircuit and error handling
-number =int(input("enter the number"))
-if(number%2==0):
-    print("even")
-else:
-    print("odd")
+#try to fetch from backend to frontend using api call 

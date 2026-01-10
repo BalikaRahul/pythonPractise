7bd17dfa5714084dd196dbefe6a3f010b3a627ba
BalikaRahul <rr7511489@gmail.com>
2026-01-10 16:01:59 +0530

    day 20

--- python.py
+++ python.py
@@ -1,5 +1,28 @@
-n= int(input("enter the number"))
-if(n%2==0):
-    print('its even number')
-else:
-    print("it is odd number")
+# def Reverse(num):
+#     rev =0
+#     while num >0:
+#         digit =num%10
+#         rev = rev*10+digit
+#         num //=10
+#     return rev
+# num=int(input("enter a number"))
+# result=Reverse(num)
+# print(result)
+# def MoveZeroToEnd(num):
+#     for i in range(len(num)):
+#         for j in range(len(num)-1):
+#             if(num[j]==0 or num[j+1]!=0):
+#                 num[j],num[j+1]=num[j+1],num[j]
+#     return num
+# num=[1,0,1,0,1,0]
+# result = MoveZeroToEnd(num)
+# print(num)
+def TargetSum(num,Target):
+    for i in range(len(num)):
+        demo=num[i]-Target
+        if(demo ==num[i-1]):
+            return "yes"
+num =[1,2,3,4,5,6]
+Target = int(input('enter the number'))
+result = TargetSum(num,Target)
+print(result)

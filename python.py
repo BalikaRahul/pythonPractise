d375871403e99bafa0bdd8d3066bea109e45cf02
BalikaRahul <rr7511489@gmail.com>
2026-02-08 12:12:28 +0530

    day 42

--- python.py
+++ python.py
@@ -638,22 +638,32 @@
 # arr = [2,3,4,4,3,3,1,5,8]
 # result =Sum(arr)
 # print(result)
-def movingAllZero(arr):
-    n=len(arr)
-    if(n<0):
-        return 0
+# def movingAllZero(arr):
+#     n=len(arr)
+#     if(n<0):
+#         return 0
+#     left =0
+#     right = n-1
+#     while left < right:
+#         if arr[left] !=0 :
+#             left+=1
+#         elif arr[right] ==0:
+#             right-=1
+#         else:
+#             arr[left],arr[right]=arr[right],arr[left]
+#             left +=1
+#             right -=1
+#     return arr
+# arr = [1,0,1,0,0,0,1]
+# result =movingAllZero(arr)
+# print(result)
+def removeDuplicate(arr):
     left =0
-    right = n-1
-    while left < right:
-        if arr[left] !=0 :
-            left+=1
-        elif arr[right] ==0:
+    right =len(arr)-1
+    while left < right :
+        if arr[left]!=arr[right]:
+            left +=1
+        elif arr[right]==arr[left]:
             right-=1
         else:
-            arr[left],arr[right]=arr[right],arr[left]
-            left +=1
-            right -=1
-    return arr
-arr = [1,0,1,0,0,0,1]
-result =movingAllZero(arr)
-print(result)
+            arr

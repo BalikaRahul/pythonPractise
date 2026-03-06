e41fcf441c05653f75a79661c48e79803834b05e
BalikaRahul <rr7511489@gmail.com>
2026-03-06 22:18:23 +0530

    nothing

--- python.py
+++ python.py
@@ -1103,12 +1103,12 @@
 #                 right+=1
 #             left +=1
 #         return right
-def noProgress(arr):
-    count=0
-    for i in range(len(arr)):
-        if arr[i]%2==0:
-            count+=1
-    return count
-arr=[2,3,4,5,6,8]
-result=noProgress(arr)
-print(result)
+# def noProgress(arr):
+#     count=0
+#     for i in range(len(arr)):
+#         if arr[i]%2==0:
+#             count+=1
+#     return count
+# arr=[2,3,4,5,6,8]
+# result=noProgress(arr)
+# print(result)

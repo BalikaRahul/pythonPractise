81f11ae812d7c4ac86f6e7b55c33d65e7a07d85e
BalikaRahul <rr7511489@gmail.com>
2026-03-16 08:06:38 +0530

    leetcode 27

--- python.py
+++ python.py
@@ -1,13 +1,29 @@
-def leetCode26(arr):
+# def leetCode26(arr):
+#     left =0
+#     right =1
+#     while right<len(arr):
+#         if arr[left]!=arr[right]:
+#             left+=1
+#             arr[left]=arr[right]
+#         right+=1
+#     return left+1
+# arr=[1,1,2]
+# result = leetCode26(arr)
+# print(result)
+# print(arr[:result])
+def leetCode27(arr,target):
     left =0
-    right =1
-    while right<len(arr):
-        if arr[left]!=arr[right]:
+    right =len(arr)-1
+    while left<right:
+        if arr[left]!=target:
             left+=1
-            arr[left]=arr[right]
-        right+=1
-    return left+1
+        elif arr[left]==target:
+            arr[left],arr[right]=arr[right],arr[left]
+            right-=1
+    return left
 arr=[1,1,2]
-result = leetCode26(arr)
+target=1
+result = leetCode27(arr,target)
 print(result)
 print(arr[:result])
+    

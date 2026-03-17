41c3589a16785196aa611ffc98c0974706ea214a
BalikaRahul <rr7511489@gmail.com>
2026-03-17 07:53:17 +0530

    day 2 leetcode 167

--- python.py
+++ python.py
@@ -49,35 +49,56 @@
 # s=["h","e","l","l","o"]
 # result = leetcode344(s)
 # print(result)
-def sorting(arr):
-    for i in range(len(arr)):
-        arr[i]=arr[i]**2
-    return arr
-    if len(arr)>1:
-        mid=len(arr)//2
-        left = arr[:mid]
-        right=arr[mid:]
-        sorting(left)
-        sorting(right)
-        i=j=k=0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                arr[k]=left[i]
-                i+=1
-            else:
-                arr[k]=right[j]
-                j+=1
-            k+=1
-        while i < len(left):
-            arr[k]=left[i]
-            i+=1
-            k+=1
-        while j< len(right):
-            arr[k]=right[j]
-            j+=1
-            k+=1
-    return arr
+# def sorting(arr):
+#     for i in range(len(arr)):
+#         arr[i]=arr[i]**2
+#     if len(arr)>1:
+#         mid=len(arr)//2
+#         left = arr[:mid]
+#         right=arr[mid:]
+#         sorting(left)
+#         sorting(right)
+#         i=j=k=0
+#         while i < len(left) and j < len(right):
+#             if left[i] < right[j]:
+#                 arr[k]=left[i]
+#                 i+=1
+#             else:
+#                 arr[k]=right[j]
+#                 j+=1
+#             k+=1
+#         while i < len(left):
+#             arr[k]=left[i]
+#             i+=1
+#             k+=1
+#         while j< len(right):
+#             arr[k]=right[j]
+#             j+=1
+#             k+=1
+#     return arr
             
-arr=[1,-2,3,4,5]
-result =sorting(arr)
+# arr=[1,-2,3,4,5]
+# result =sorting(arr)
+# print(result)
+# def leetcode(arr,target):
+#     for i in range(len(arr)):
+#         for j in range(i+1,len(arr)):
+#             if arr[i]+arr[j]==target:
+#                 return i,j
+#     return "not found "
+def leetcode167(arr,target):
+    left =0
+    right=len(arr)-1
+    while left<right:
+        total = arr[left]+arr[right]
+        if total ==target:
+            return left+1,right+1
+        elif total < target:
+            left+=1
+        else:
+            right-=1
+    return "not found"
+arr=[2,3,4]
+target=6
+result = leetcode167(arr,target)
 print(result)

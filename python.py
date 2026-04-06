a6de89e67804cf23a278fd1e589570596f605121
BalikaRahul <rr7511489@gmail.com>
2026-04-06 23:39:13 +0530

    leetcode1752

--- python.py
+++ python.py
@@ -292,3 +292,11 @@
 # nums =[3,1,-2,-5,2,-4]
 # result=leetcode2149(nums)
 # print(result)
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        count =0
+        n=len(nums)
+        for i in range(n):
+            if nums[i]>nums[(i+1)%n]:
+                count+=1
+        return count<=1

953fc5cd8e5c8ddc04edbcf22e5df4ca5ae37edd
BalikaRahul <rr7511489@gmail.com>
2026-04-18 23:48:37 +0530

    leetcode3

--- python.py
+++ python.py
@@ -470,21 +470,57 @@
 # x=123
 # result =leetcode9
 # print(result)
-def waterProblem(nums):
-    left =0
-    right=len(nums)-1
-    maxi=0
-    while left<right:
-        cur =min(nums[left],nums[right])*(right-left)
-        if cur>maxi:
-            maxi =cur
-        elif nums[left]<nums[right]:
+# def waterProblem(nums):
+#     left =0
+#     right=len(nums)-1
+#     maxi=0
+#     while left<right:
+#         cur =min(nums[left],nums[right])*(right-left)
+#         if cur>maxi:
+#             maxi =cur
+#         elif nums[left]<nums[right]:
+#             left+=1
+#         else:
+#             right-=1
+#     return maxi
+# nums=[1,8,6,2,5,4,8,3,7]
+# result=waterProblem(nums)
+# print(result)
+def leetcode3(s):
+    left=0
+    res=0
+    seen=set()
+    for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
             left+=1
-        else:
-            right-=1
-    return maxi
-nums=[1,8,6,2,5,4,8,3,7]
-result=waterProblem(nums)
+        seen.add(s[right])
+        res=max(res,right-left+1)
+    return res
+s='abcabcbb'
+result =leetcode3(s)
 print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     

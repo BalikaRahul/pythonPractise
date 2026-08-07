955fdf42932575313c67c374fcda8485c4218128
BalikaRahul <rr7511489@gmail.com>
2026-08-07 10:58:18 +0530

    update

--- Day 7 Challenge.py
+++ Day 7 Challenge.py
@@ -11,31 +11,31 @@ if count==2:
 else:
     print('not a prime number')
 # Task 2 using recursion
-# def fib(n):
-#     if n<=1:
-#         return n
-#     return fib(n-1)+fib(n-2)
-# n=int(input('enter the number: '))
-# result = fib(n)
-# print(result)
+def fib(n):
+    if n<=1:
+        return n
+    return fib(n-1)+fib(n-2)
+n=int(input('enter the number: '))
+result = fib(n)
+print(result)
 # without function
-# n=int(input('enter the number: '))
-# a=0
-# b=1
-# for i in range(1,n+1):
-#     print(a,end=' ')
-#     c=a+b
-#     a=b
-#     b=c
+n=int(input('enter the number: '))
+a=0
+b=1
+for i in range(1,n+1):
+    print(a,end=' ')
+    c=a+b
+    a=b
+    b=c
 # Task 4
-# while True:
-#     num = int(input('enter the number: '))
-#     if num == 3:
-#         print(f'you have guessed the number :{num}')
-#         break
-#     elif num >3:
-#         print('the number is lower than your guess  ')
-#     elif num< 3:
-#         print('the number is higher than your guess ')
-#     else:
-#         print('wrong guess')
+while True:
+    num = int(input('enter the number: '))
+    if num == 3:
+        print(f'you have guessed the number :{num}')
+        break
+    elif num >3:
+        print('the number is lower than your guess  ')
+    elif num< 3:
+        print('the number is higher than your guess ')
+    else:
+        print('wrong guess')
